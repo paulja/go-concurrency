6375131ad4331ee1ad0c6636ef3bb2602a88bf85
PaulJ <paulj@agylia.com>
2024-11-21 17:28:58 +0000

    update to make fib pre-emptive

--- fib/main.go
+++ fib/main.go
@@ -1,21 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"time"
+)
 
 func main() {
-	var fib func(int) <-chan int
-	fib = func(n int) <-chan int {
-		res := make(chan int)
-		go func() {
-			defer close(res)
-			if n <= 2 {
-				res <- 1
-				return
-			}
-			res <- <-fib(n-1) + <-fib(n-2)
-		}()
-		return res
+	var num int
+	flag.IntVar(&num, "n", 0, "number param")
+	flag.Parse()
+
+	done := make(chan interface{})
+	defer close(done)
+
+	select {
+	case n := <-fib(done, num):
+		fmt.Printf("fib(%d) = %d\n", num, n)
+	case <-time.Tick(5 * time.Second):
+		done <- true
+		fmt.Printf("request timed out (%d)\n", num)
 	}
-	x := 30
-	fmt.Printf("fib(%d) = %d", x, <-fib(x))
+}
+
+func fib(done <-chan interface{}, n int) <-chan int {
+	res := make(chan int)
+	go func() {
+		defer close(res)
+
+		select {
+		case <-done:
+			return
+		default:
+		}
+
+		if n <= 2 {
+			res <- 1
+			return
+		}
+		res <- <-fib(done, n-1) + <-fib(done, n-2)
+	}()
+	return res
 }
